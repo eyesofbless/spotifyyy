1251eec776bfdd09af8ca16942e73b8e6049f251
eyesofbless <135031473+eyesofbless@users.noreply.github.com>
2023-12-25 06:13:30 +0300

    providers for auth and supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,33 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Figtree} from 'next/font/google'
 import './globals.css'
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen me!!!',
+    title: 'Spotify Clone',
+    description: 'Listen me!!!',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>
-      <Sidebar>
-          {children}
-      </Sidebar>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={font.className}>
+        <SupabaseProvider>
+            <UserProvider>
+                <Sidebar>
+                    {children}
+                </Sidebar>
+            </UserProvider>
+        </SupabaseProvider>
+        </body>
+        </html>
+    )
 }
